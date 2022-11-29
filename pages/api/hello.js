afb8ed25c5c404c9920a9585d6a114bd0c433b1d
odedbendov <odedbendov@gmail.com>
2022-11-29 23:06:36 +0200

    testing api route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from 'fs';
+import { emojisLocalFolder } from '../../components/Globals';
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const files = fs.readdirSync(emojisLocalFolder, { withFileTypes: true });
+  res.status(200).json({ name: files.map(f => f.name) })
 }
